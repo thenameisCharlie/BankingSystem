3ad71dca9fb2c6bcbfc8a916e0a24f9c67e43c50
CarlosVillatoro <a7xcharlie18@gmail.com>
2025-04-27 15:53:29 -0400

    Created BankAccount class

---
+++ Banking System.py
@@ -0,0 +1,28 @@
+#Checking system class
+import random
+
+class BankAccount: 
+    def __init__(self, accountNum, customerName, balance):
+        self.accountNum = BankAccount._account_num_randomizer()
+        self.customerName = customerName
+        self.balance = balance
+
+    #Function creates a random account number for the user
+    @staticmethod
+    def _account_num_randomizer():
+        random.randint(1000, 5000)
+    
+    #Function that deposits money to account
+    def deposit(self, amount):
+        self.balance += amount
+    
+    #Function that withdraws money from account
+    def withdraw(self, amount):
+        self.balance -= amount
+    
+    #Function to check balance
+    def check_balance(self):
+        return self.balance
+
+#class CheckingAccount(BankAccount):
+    
