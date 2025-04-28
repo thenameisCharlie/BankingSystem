45587f813df6b81515c228d34f1bb4562dadbdae
Charlie <143125248+CarlosVillatoro1995@users.noreply.github.com>
2025-04-28 14:33:05 -0400

    Completed the checkingAccount class

--- Banking System.py
+++ Banking System.py
@@ -2,6 +2,7 @@
 import random
 
 class BankAccount: 
+    #constructor (How you create an instanace of an object)
     def __init__(self, customerName, balance):
         self.accountNum = BankAccount._account_num_randomizer()
         self.customerName = customerName
@@ -24,5 +25,31 @@ class BankAccount:
     def check_balance(self):
         return self.balance
 
-#class CheckingAccount(BankAccount):
+class CheckingAccount(BankAccount):
+    #Overdraft fee for accounts that are in the negative
+    def overdraft_fee(self):
+        if self.balance < 0:
+            self.balance -= 35
+
+    #Override function that withdraws money from the account and adds an overdraft fee if account is under 0
+    def withdraw(self, amount):
+        super().withdraw(amount) #calls the parents withdraw method
+        self.overdraft_fee()
+    
+    #Function that transfers funds from checking to savings
+    def transfer_to_savings(self, amount, savingsAccount):
+        if self.balance >= amount: 
+            self.balance -= amount
+            savingsAccount.balance += amount
+
+
+#class SavingsAccount(BankAccount):
+
+    
+
+
+
+    
+    
+
     
