a8adfab76511b9bbe98b98f76622881577df1ec7
Charlie <143125248+CarlosVillatoro1995@users.noreply.github.com>
2025-05-10 15:49:52 -0400

    updated the input options along with all the test cases

--- Banking System.py
+++ Banking System.py
@@ -60,23 +60,77 @@ class SavingsAccount(BankAccount):
 #When you re-run an object it destroys the old object in the memory and creates a new one
 customerName = input("Please enter your name: ")
 
-bankAccount1 = BankAccount(customerName)
+bankAccount1 = CheckingAccount(customerName)
+bankAccount2 = SavingsAccount(customerName)
+customerOptions = 0
 
-customerOptions = input(" 1. Deposit \n 2. Withdraw \n 3. Transfer \n 4. Open new account \n 5. Check Balance")
+while customerOptions != 6:
+    customerOptions = int(input(f"What would you like to do {customerName}?\n 1. Deposit \n 2. Withdraw \n 3. Transfer \n 4. Open new account \n 5. Check Balance\n 6. Exit\n\n"))
 
+    if customerOptions == 1:
+        depositAmount = int(input("How much would you like to deposit? "))
+        bankAccount1.deposit(depositAmount)
 
-if customerOptions == 1:
-    depositAmount = input("How much would you like to deposit? ")
-    bankAccount1.deposit(depositAmount)
+    elif customerOptions == 2: 
+        withdrawAmount = int(input("How much would you like to withdraw? "))
+        bankAccount1.withdraw(withdrawAmount)
 
+    elif customerOptions == 3:
+        #This while loop along with the if statement simulates a do while loop
+        while True:
+            accountOption = int(input("Which account would you like to transfer to?\n 1. Checking\n 2. Savings\n "))
 
-# accountType = input("What type of account would you like to open (Checking or Savings)? ").lower()
-# customerName = input("Please provide your full name: ")
-# balanceAmount = input("Enter a balance amount for your new account: ")
+            if accountOption == 1:
+                transferAmount = int(input("What is the amount you would like to transfer? "))
+                bankAccount2.transfer_to_checking(transferAmount, bankAccount1)
+                break
+            
+            elif accountOption == 2:
+                transferAmount = int(input("What is the amount you would like to transfer? "))
+                bankAccount1.transfer_to_savings(transferAmount, bankAccount2)
+                break
+            
+            else:
+                print("Invalid entry. Please try again")
 
-# if accountType == "checking" or accountType == "savings": 
-#     print("This is correct!")
+    elif customerOptions == 4:
+        
+        while True:
+            accountType = int(input("What type of account would you like to open (\n 1. Checking\n 2. Savings\n)? "))
 
-# else:
-#     print("Invalid entry. Please try again")
+            
+            if accountType == 1:
+                bankAccount3 = CheckingAccount(customerName)
+                print("\nNew checking account created!")
+                break
+
+            elif accountType == 2: 
+                bankAccount4 = SavingsAccount(customerName)
+                print("\nNew savings account created!")
+                break
+            
+            else:
+                print("Invalid entry. Please try again")
+
+    elif customerOptions == 5:
+        
+        while True: 
+            accountBalanceCheck = int(input("Which account would you like to check \n 1. Checking\n 2. Savings \n 3. All"))
+
+            if accountBalanceCheck == 1: 
+                print(f"\n {bankAccount1.check_balance()}")
+                break
+
+            elif accountBalanceCheck == 2:
+                print(f"\n {bankAccount2.check_balance()}")
+                break
+            
+            elif accountBalanceCheck == 3:
+                print(f"\n {bankAccount1.check_balance()}\n")
+                print(f"{bankAccount2.check_balance()}")
+                break
+        
+            else:
+                print("Invalid entry, please try again")
+
 
