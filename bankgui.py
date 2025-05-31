5c11d4544052a81480c887efe50a7cbb182ad439
CarlosVillatoro <a7xcharlie18@gmail.com>
2025-05-30 22:31:37 -0400

    updated the GUI screen to deposit money for the user

--- bankgui.py
+++ bankgui.py
@@ -1,28 +1,51 @@
 import tkinter as tk
+from savings_account import SavingsAccount
+from checking_account import CheckingAccount
 
 
 def display_gui():
 
-    def show_menu(customerName):
+    def clearScreen():
         #Destroys all the widgets on the screen before continuing to the next interface
         for widget in guiScreen.winfo_children():
             widget.destroy()
 
-        tk.Label(guiScreen, text="Welcome to Richfolk Bank!").grid(row=0, column=0, pady=20) 
+
+    def handle_option(selected, bankAccount1):
+       clearScreen()
+
+       if selected == 1:
+            tk.Label(guiScreen, text="How much would you like to deposit? ").grid(row=0, column=0, pady=20) 
+
+            deposit_amount = tk.Entry(guiScreen, width=30)
+            deposit_amount.grid(row=1, column=0, pady=(0, 10))
+
+            def confirm_deposit():
+                try:
+                    amount_conversion = float(deposit_amount.get())
+                    bankAccount1.deposit(amount_conversion)
+                    tk.Label(guiScreen, text=f"${amount_conversion} deposited successfully!").grid(row=3, column=0)
+                except ValueError:
+                    tk.Label(guiScreen, text="Invalid amount. Please enter a number.").grid(row=3, column=0)
+
+            tk.Button(guiScreen, text="Confirm", command=confirm_deposit).grid(row=2, column=0, pady=(0, 10))
+
+
+    
+    def show_menu(customerName):
+        clearScreen()
         
         optionSelected = tk.IntVar()
 
-        tk.Label(guiScreen, text=f"What would you like to do {customerName}").grid(row=0, column=0, pady=20) 
+        tk.Label(guiScreen, text=f"Welcome to Richfolk Bank, {customerName}! ").grid(row=0, column=0, pady=20) 
 
-        #Create radio buttons
-        tk.Radiobutton(guiScreen, text="Deposit", variable=optionSelected, value=1).grid(row=1, column=0, columnspan=2, pady=5)
-        tk.Radiobutton(guiScreen, text="Withdraw", variable=optionSelected, value=2).grid(row=2, column=0, columnspan=2, pady=5)
-        tk.Radiobutton(guiScreen, text="Transfer", variable=optionSelected, value=3).grid(row=3, column=0, columnspan=2, pady=5)
-        tk.Radiobutton(guiScreen, text="Open new account", variable=optionSelected, value=4).grid(row=4, column=0, columnspan=2, pady=5)
-        tk.Radiobutton(guiScreen, text="Check balance", variable=optionSelected, value=5).grid(row=5, column=0, columnspan=2, pady=5)
-        tk.Radiobutton(guiScreen, text="Exit", variable=optionSelected, value=6).grid(row=6, column=0, columnspan=2, pady=5)
+        options = [("Deposit", 1), ("Withdraw", 2), ("Transfer", 3), ("Open new account", 4), ("Check balance", 5), ("Exit", 6)]
 
-        tk.Button(guiScreen, text="Continue", command=guiScreen.destroy).grid(row=4, column=0, columnspan=2, pady=10)
+        for idx, (text, val) in enumerate(options):
+            tk.Radiobutton(guiScreen, text=text, variable=optionSelected, value=val).grid(row=idx + 1, column=0, pady=2)
+
+        tk.Button(guiScreen, text="Continue", command=lambda: handle_option(optionSelected.get(), bankAccount1)).grid( 
+        row=len(options) + 1, column=0, pady=(10, 0)) #lambda creates an anonymous one time function that doesnt run until button is pressed
 
     #handles the GUI screen
     guiScreen = tk.Tk()
@@ -47,6 +70,11 @@ def display_gui():
     nameEntry = tk.Entry(centerFrame, width=30)
     nameEntry.grid(row=1, column=0, pady=(0, 10))
     tk.Button(centerFrame, text="Enter", command=lambda:show_menu(nameEntry.get())).grid(row=2, column=0)
+
+    #Bank account generated after entering name
+    bankAccount1 = CheckingAccount(nameEntry.get())
+    bankAccount2 = SavingsAccount(nameEntry.get())
+    
    
 
     guiScreen.mainloop()
